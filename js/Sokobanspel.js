e3a25393393f3ec747d6b3d0b6818e14e1e7d470
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-08-16 17:20:40 +0200

    addeventlistener

--- js/Sokobanspel.js
+++ js/Sokobanspel.js
@@ -1,2 +1,44 @@
-document.addEventListener( "", () => {
-};
+// Time-stamp: <2021-08-16 17:20:12 stefan>
+//
+
+"use strict"
+
+var spelplankontext;
+var bredd;
+var höjd;
+
+var nyckelpiganÅtVänster, nyckelpiganÅtHöger, nyckelpiganUppåt, nyckelpiganNedåt;
+
+function init() {
+    let spelplan=document.createElement( "canvas");
+    spelplan.width=800;
+    spelplan.height=500;
+
+     if (spelplan.getContext) {
+	 spelplankontext = spelplan.getContext("2d");
+	 spelplankontext.fileStyle = "#000000";
+     }
+
+    let vy=document.getElementById( "vy");
+    vy.appendChild(spelplan);
+
+    nyckelpiganUppåt=document.createElement("img");
+    nyckelpiganUppåt.src="png/hraci1.png";
+
+    nyckelpiganÅtHöger=document.createElement("img");
+    nyckelpiganÅtHöger.src="png/hraci2.png";
+
+    nyckelpiganNedåt=document.createElement("img");
+    nyckelpiganNedåt.src="png/hraci3.png";
+
+    nyckelpiganÅtVänster=document.createElement("img");
+    nyckelpiganÅtVänster.src="png/hraci4.png";
+
+    document.addEventListener( "keydown", tangenttryck, false);
+}
+
+document.addEventListener( "DOMContentLoaded", init, false);
+
+function tangenttryck() {
+    console.log("keypress");
+}
